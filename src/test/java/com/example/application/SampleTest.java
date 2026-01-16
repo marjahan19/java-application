21e014ed7cfb3ba23aab50a5861592ccb175fe76
marjahan19 <marjahanaktherapon@gmail.com>
2026-01-16 16:39:41 +0000

    SampleTest for Jenkins pipeline

---
+++ src/test/java/com/example/application/SampleTest.java
@@ -0,0 +1,12 @@
+package com.example.application;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+
+    @Test
+    public void simpleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
